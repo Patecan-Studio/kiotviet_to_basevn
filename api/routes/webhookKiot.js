36482d9555caac79c8043a24b9808b289a4414b5
Thien Trong <tranthientrong.it@gmail.com>
2023-03-20 14:22:46 +0700

    [ADD] Sent Wedhook KiotViet to Ftiles BE

--- api/routes/webhookKiot.js
+++ api/routes/webhookKiot.js
@@ -9,6 +9,17 @@ router.post('/webhook', function(req, res) {
 
     console.log(log(`RECEIVED INVOICE WEBHOOK: `)+ '\n' +`${JSON.stringify(req.body)}` +'\n')
 
+    let body = JSON.stringify(req.body);
+    fetch('https://c6f26d81bc51.ngrok.app/receiverPort/invoiceEvent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body
+    }).then(r =>{
+        console.log("ALREADY SENT WEBHOOK EVENT TO ftiles")
+    });
+
     handleWebhookInvoice(req, res).then(function (result) {
         console.log(result)
         res.status(200).send(result)
