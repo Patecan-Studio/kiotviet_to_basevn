5d3e61848d0d79f229094e567ab233fda08aa47c
Thien Trong <tranthientrong.it@gmail.com>
2023-03-30 23:40:30 +0700

    [Add] Mapping KiotViet User to Base user by email

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ import webhookKiotRouter from "./api/routes/webhookKiotRoute.js";
 import webhookBaseRouter  from "./api/routes/webhookBaseRoute.js";
 import productRouter from "./api/routes/product.js";
 import {log} from "./settings/logger.js";
+import cors from 'cors';
 
 const app = express()
-const PORT = 3000
+app.use(cors())
+const port = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV;
+const mySetting = process.env.MY_SETTING;
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
@@ -18,7 +22,7 @@ app.use("/base/webhook", webhookBaseRouter)
 app.use("/api/product", productRouter);
 
 
-app.listen(PORT, (err) => {
+app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
