3272493a4b201df5eda9f1b80f78c90083aee2ab
Thien Trong <tranthientrong.it@gmail.com>
2023-04-01 00:37:15 +0700

    [UPDATE] Add Sentry

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 import webhookKiotRouter from "./api/routes/webhookKiotRoute.js";
 import webhookBaseRouter  from "./api/routes/webhookBaseRoute.js";
 import productRouter from "./api/routes/product.js";
@@ -8,6 +7,12 @@ import {log} from "./settings/logger.js";
 import cors from 'cors';
 import * as Sentry from "@sentry/node";
 import * as Tracing from "@sentry/node";
+
+
+const port = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV;
+const mySetting = process.env.MY_SETTING;
+
 
 const app = express()
 
@@ -17,35 +22,43 @@ Sentry.init({
     integrations: [
         new Sentry.Integrations.Http({ tracing: true }),
         new Tracing.Integrations.Express({ app }),
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
     ],
     tracesSampleRate: 1.0,
 });
+
 app.use(cors())
-
-
-const port = process.env.PORT || 3000;
-const nodeEnv = process.env.NODE_ENV;
-const mySetting = process.env.MY_SETTING;
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.requestHandler({
+    ip: true,
+}));
 app.use(Sentry.Handlers.tracingHandler());
-app.use(Sentry.Handlers.errorHandler());
 
-app.use(function onError(err, req, res, next) {
-    res.statusCode = err.statusCode;
-    res.end(res.sentry + "\n");
-});
+
+
 
 app.use("/kiotviet/webhook", webhookKiotRouter)
 app.use("/base/webhook", webhookBaseRouter)
 app.use("/api/product", productRouter);
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/testConnection", (req, res) => res.status(200).send("Hello!!!"));
 
+
+app.use(Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+        return true;
+    }
+}));
+
+app.use(function onError(err, req, res, next) {
+    res.statusCode = err.statusCode;
+    res.end(res.sentry + "\n");
+});
 
 app.listen(port, (err) => {
     if (err) {
